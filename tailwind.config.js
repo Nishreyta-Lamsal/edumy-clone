6b60d6a9fc9905f17c9d4bd7f1dc48c54640d25d
Nishreyta-Lamsal <nishreytal@gmail.com>
2024-08-26 18:57:51 +0545

    Setting up everything required for building the website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-green': '#82b440',
+      },
+    },
+  },
+  plugins: [],
+}
+
